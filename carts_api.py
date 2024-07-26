ccdf34c4d79ab902edf78cb3a803302da6114023
ALIAKBAR0211 <hackerbegako@gmail.com>
2024-07-26 16:07:24 +0300

    написан код для carts

--- carts_api.py
+++ carts_api.py
@@ -6,32 +6,20 @@ def get_all_carts():
         products = response.json()
         return products
     
-def get_a_single_cart():
-    response = requests.get('https://fakestoreapi.com/carts/5')
+def get_single_cart(cart_id):
+    response = requests.get(f'https://fakestoreapi.com/carts/{cart_id}')
     if response.status_code == 200:
         products = response.json()
         return products
 
-def get_limit_results():
-    response = requests.get('https://fakestoreapi.com/carts?limit=5')
+def get_limit_results(limit):
+    response = requests.get(f'https://fakestoreapi.com/carts?limit={limit}')
     if response.status_code == 200:
         products = response.json()
         return products
     
-def get_sort_results():
-    response = requests.get('https://fakestoreapi.com/carts?sort=desc')
-    if response.status_code == 200:
-        products = response.json()
-        return products
-    
-def get_carts_in_a_date_range():
-    response = requests.get('https://fakestoreapi.com/carts?startdate=2019-12-10&enddate=2020-10-10')
-    if response.status_code == 200:
-        products = response.json()
-        return products
-    
-def get_user_carts():
-    response = requests.get('https://fakestoreapi.com/carts/user/2')
+def get_sort_results(sort_type):
+    response = requests.get(f'https://fakestoreapi.com/carts?sort={sort_type}')
     if response.status_code == 200:
         products = response.json()
         return products
